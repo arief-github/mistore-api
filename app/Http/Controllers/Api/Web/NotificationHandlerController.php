a90bd8539fac502f56f4c3abce57b509eb61e4a7
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2024-04-08 11:22:48 +0700

    feat: add invoices api

--- app/Http/Controllers/Api/Web/NotificationHandlerController.php
+++ app/Http/Controllers/Api/Web/NotificationHandlerController.php
@@ -2,91 +2,107 @@
 
 namespace App\Http\Controllers\Api\Web;
 
-use App\Http\Controllers\Controller;
 use App\Models\Invoice;
-use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class NotificationHandlerController extends Controller
 {
     /**
-     *
      * index
+     *
+     * @param  mixed $request
+     * @return void
      */
-
     public function index(Request $request)
     {
-        $payload = $request->getContent();
+        $payload      = $request->getContent();
         $notification = json_decode($payload);
-        $validSignatureKey = hash("sha256", $notification->order_id . $notification->status_code . $notification->gross_amount . config('services.midtrans.serverKey'));
 
-        if($notification->signature_key != $validSignatureKey) {
-          return response(['message' => 'Invalid signature'], 403);
+        $validSignatureKey = hash("sha512", $notification->order_id . $notification->status_code . $notification->gross_amount . config('services.midtrans.serverKey'));
+
+        if ($notification->signature_key != $validSignatureKey) {
+            return response(['message' => 'Invalid signature'], 403);
         }
 
-        $transaction = $notification->transaction_status;
-        $type = $notification->payment_type;
-        $orderId = $notification->order_id;
+        $transaction  = $notification->transaction_status;
+        $type         = $notification->payment_type;
+        $orderId      = $notification->order_id;
 
-        // data transaction
+        //data tranaction
         $data_transaction = Invoice::where('invoice', $orderId)->first();
 
         if ($transaction == 'capture') {
-           if ($type == 'credit_card') {
 
-           }
-        } else if($transaction == 'settlement') {
+            // For credit card transaction, we need to check whether transaction is challenge by FDS or not
+            if ($type == 'credit_card') {
+
+                //nothing
+
+            }
+
+        } elseif ($transaction == 'settlement') {
+
             /**
-             * update invoice to success
+             *   update invoice to success
              */
-
             $data_transaction->update([
-               'status' => 'success'
+                'status' => 'success'
             ]);
 
-            // update stock product
-            foreach ($data_transaction->orders()->get() as $order) {
+            //update stock product
+            foreach($data_transaction->orders()->get() as $order) {
+
                 $product = Product::whereId($order->product_id)->first();
                 $product->update([
                     'stock' => $product->stock - $order->qty
                 ]);
             }
-        } else if ($transaction == 'pending') {
-            /**
-             *
-             * update invoice to pending
-             */
 
-            $data_transaction->update([
-               'status' => 'pending'
-            ]);
-        } else if ($transaction == 'deny') {
-            /**
-             *
-             * update invoice to failed
-             */
 
-            $data_transaction->update([
-               'status' => 'failed'
-            ]);
-        } else if ($transaction == 'expire') {
-            /**
-             *
-             * update invoice to expired
-             */
+        } elseif($transaction == 'pending'){
 
-            $data_transaction->update([
-               'status' => 'expired'
-            ]);
-        } else if ($transaction == 'cancel') {
+
             /**
-             *
-             * update invoice to failed
-             *
+             *   update invoice to pending
+             */
+            $data_transaction->update([
+                'status' => 'pending'
+            ]);
+
+
+        } elseif ($transaction == 'deny') {
+
+
+            /**
+             *   update invoice to failed
              */
             $data_transaction->update([
                 'status' => 'failed'
             ]);
+
+
+        } elseif ($transaction == 'expire') {
+
+
+            /**
+             *   update invoice to expired
+             */
+            $data_transaction->update([
+                'status' => 'expired'
+            ]);
+
+
+        } elseif ($transaction == 'cancel') {
+
+            /**
+             *   update invoice to failed
+             */
+            $data_transaction->update([
+                'status' => 'failed'
+            ]);
+
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -103,5 +103,9 @@ Route::prefix("admin")->group(function () {
 
         // read customers
         Route::get('/customers', [App\Http\Controllers\Api\Admin\CustomerController::class, 'index', ['as' => 'admin']]);
+
+        //invoices resource
+        Route::apiResource('/invoices', App\Http\Controllers\Api\Admin\InvoiceController::class, ['except' => ['create', 'store', 'edit', 'update', 'destroy'], 'as' => 'admin']);
+
     });
 });
